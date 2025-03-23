ccba2061d795cbd921d6b82eed2f578f0df5dd9a
notCroptu <martinsmariana1972@gmail.com>
2025-03-23 12:46:42 +0000

    better reading of chooser switch

--- Assets/Scripts/Tabletop/Pathfind/Chooser/PathfinderChooser.cs
+++ Assets/Scripts/Tabletop/Pathfind/Chooser/PathfinderChooser.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 /// </summary>
 public class PathfinderChooser
 {
-    public static Pathfinder ChooseRange(MonoBehaviour other, PathfinderType rangeType)
+    public static Pathfinder ChooseRange(MonoBehaviour other, PathfinderType rangeType, bool nonAvoid = false)
     {
         return rangeType switch
         {
-            PathfinderType.AStar => new AStarPathfinder(other, false),
-            PathfinderType.OpenStar => new OpenStarPathfinder(other, false),
-            PathfinderType.BFSRange => new BFSRangePathfinder(other, false),
-            PathfinderType.HexagonRange => new HexRangePathfinder(other, false),
-            PathfinderType.StarRange => new StarRangePathfinder(other, false),
-            
+            PathfinderType.AStar => new AStarPathfinder(other, nonAvoid),
+            PathfinderType.OpenStar => new OpenStarPathfinder(other, nonAvoid),
+            PathfinderType.BFSRange => new BFSRangePathfinder(other, nonAvoid),
+            PathfinderType.HexagonRange => new HexRangePathfinder(other, nonAvoid),
+            PathfinderType.StarRange => new StarRangePathfinder(other, nonAvoid),
+
             // fallback on AStar
-            _ => new AStarPathfinder(other, false),
+            _ => new AStarPathfinder(other, nonAvoid),
         };
     }
 }
