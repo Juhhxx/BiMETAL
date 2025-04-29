3405c5b3dbf1fa334ba011489d898576e26e3fe9
notCroptu <martinsmariana1972@gmail.com>
2025-04-29 16:53:24 +0100

    cell information script for tabletop ui

---
+++ Assets/Scripts/Tabletop/UI/CellInfo.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+
+public class CellInfo : MonoBehaviour
+{
+    [SerializeField] private GameObject _cellGO;
+    [SerializeField] private GameObject _pieceGO;
+    [SerializeField] private TMP_Text _cellType;
+    [SerializeField] private TMP_Text _cellWeight;
+    [SerializeField] private TMP_Text _pieceType;
+    [SerializeField] private TMP_Text _pieceRange;
+    [SerializeField] private TMP_Text _pieceMod;
+
+    public void Hover(HexagonCell cell = null)
+    {
+        if ( cell == null )
+        {
+            _cellGO.SetActive(false);
+            _pieceGO.SetActive(false);
+
+            _pieceRange.gameObject.SetActive(false);
+            _pieceMod.gameObject.SetActive(false);
+
+            return;
+        }
+
+        _cellGO.SetActive(true);
+
+        _cellType.text = "Cell";
+        if ( cell.Modifier != null )
+            _cellType.text = cell.Modifier.name + " Modified " + _cellType.text;
+
+        _cellWeight.text = "Movement Cost: " + cell.Weight;
+
+        if ( cell.Piece != null )
+        {
+            _pieceGO.SetActive(true);
+
+            _pieceType.text = cell.Piece.Name + " Piece";
+
+            PieceInteractive piece = cell.Piece as PieceInteractive;
+            if (piece != null)
+            {
+                if ( piece.HasModifier )
+                {
+                    _pieceMod.gameObject.SetActive(true);
+                    _pieceRange.text = "Modifier: " + piece.Modifier.name;
+                }
+                _pieceRange.gameObject.SetActive(true);
+                _pieceRange.text = "Movement Points: " + piece.Movement.Points;
+            }
+        }
+    }
+
+}
