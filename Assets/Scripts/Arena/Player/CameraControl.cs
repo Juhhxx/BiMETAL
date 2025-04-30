ee453791d7bf688c1200bbdee3168648fcc235ad
Julia Costa <juliacosta232005@gmail.com>
2025-04-30 14:20:25 +0100

    Fix Material Issues

--- Assets/Scripts/Arena/Player/CameraControl.cs
+++ Assets/Scripts/Arena/Player/CameraControl.cs
@@ -19,7 +19,6 @@ public class CameraControl : MonoBehaviour
     [SerializeField] private Transform  _deocclusionPivot;
     [SerializeField] private LayerMask  _deocclusionLayerMask;
     [SerializeField] private float      _deocclusionThreshold;
-    [SerializeField] private float      _deocclusionSpeed;
     
     private Vector3 _deocclusionVector;
     private Vector3 _deocclusionPoint;
@@ -102,23 +101,34 @@ public class CameraControl : MonoBehaviour
     }
     private void PreventOcclusion()
     {
-        _deocclusionPoint = _cameraTransform.position - _cameraTransform.TransformDirection(_deocclusionVector);
+        _deocclusionPoint = _cameraTransform.position;
+        _deocclusionPoint.z = _zoomPosition;
+
+        Debug.DrawLine(_deocclusionPivot.position, _deocclusionPoint);
 
         if (Physics.Linecast(_deocclusionPivot.position, _deocclusionPoint, out RaycastHit hitInfo, _deocclusionLayerMask.value))
         {
             _cameraTransform.position = hitInfo.point + _cameraTransform.TransformDirection(_deocclusionVector);
+
+            Vector3 localPosition = _cameraTransform.localPosition;
+            localPosition.x = localPosition.y = 0f;
+            _cameraTransform.localPosition = localPosition;
         }
         else
+        {
             RevertDeocclusion();
+        }
     }
     private void RevertDeocclusion()
     {
+        Debug.LogWarning("RESSETING ZOOM");
         _position = _cameraTransform.localPosition;
 
         if (_position.z > _zoomPosition)
         {
             // _position.z = Mathf.Max(_position.z - _deocclusionSpeed * Time.deltaTime, _zoomPosition);
             // _deocclusionPoint = transform.TransformPoint(_position) - _cameraTransform.TransformDirection(_deocclusionVector);
+            Debug.LogWarning("RESSETING ZOOM 2");
             _position.z = _zoomPosition;
 
             if (!Physics.Linecast(_deocclusionPivot.position, _deocclusionPoint))
