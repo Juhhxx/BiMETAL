c2e6540a778b669160151c592da9785c665189a9
Julia Costa <juliacosta232005@gmail.com>
2025-05-03 17:18:01 +0100

    Add Enemy Movement Script

---
+++ Assets/Scripts/Arena/AI/EnemyMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+using NaughtyAttributes;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [OnValueChanged("UpdateStats")] [SerializeField] private float _maxSpeed;
+    [OnValueChanged("UpdateStats")] [SerializeField] private float _maxChaseSpeed;
+    [OnValueChanged("UpdateStats")] [SerializeField] private float _maxAngularSpeed;
+    [OnValueChanged("UpdateStats")] [SerializeField] private float _targetAcceptanceRadius;
+
+    public float MaxSpeed => _maxSpeed;
+    public float MaxChaseSpeed => _maxChaseSpeed;
+    public float AcceptanceRadius => _targetAcceptanceRadius;
+
+    private NavMeshAgent _agent;
+
+    private void UpdateStats()
+    {
+        if(Application.isPlaying)
+        {
+            _agent.speed            = _maxSpeed;
+            _agent.angularSpeed     = _maxAngularSpeed;
+            _agent.stoppingDistance = _targetAcceptanceRadius;
+        }
+    }
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        _agent.speed            = _maxSpeed;
+        _agent.angularSpeed     = _maxAngularSpeed;
+        _agent.stoppingDistance = _targetAcceptanceRadius;
+        _agent.updateRotation = true;
+    }
+
+    public void SetDestination(Vector3 target)
+    {
+        // _agent.updateRotation = (target - transform.position).normalized;
+        _agent.destination = target;
+    }
+    public void SetSpeed(float speed) => _agent.speed = speed;
+    public void SetAcceptanceRadius(float radius) => _agent.stoppingDistance = radius;
+}
