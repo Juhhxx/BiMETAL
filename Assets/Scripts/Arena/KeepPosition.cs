3f7490c28ce5be0970395ce6bebb1ae60bfc74dc
Julia Costa <juliacosta232005@gmail.com>
2025-05-04 02:41:41 +0100

    Edit KeepPosition Script - Editor Quality of Life

--- Assets/Scripts/Arena/KeepPosition.cs
+++ Assets/Scripts/Arena/KeepPosition.cs
@@ -1,25 +1,39 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class KeepPosition : MonoBehaviour
 {
-    [SerializeField] private Vector3    _position;
     [SerializeField] private bool       _keepX;
+    [ShowIf(EConditionOperator.And, "KeepX", "NotChangeAll")][SerializeField] private float _xValue;
     [SerializeField] private bool       _keepY;
+    [ShowIf(EConditionOperator.And, "KeepY", "NotChangeAll")][SerializeField] private float _yValue;
     [SerializeField] private bool       _keepZ;
-    [SerializeField] private bool       _localPosition;
+    [ShowIf(EConditionOperator.And, "KeepZ", "NotChangeAll")][SerializeField] private float _zValue;
+    private bool KeepX => _keepX;
+    private bool KeepY => _keepY;
+    private bool KeepZ => _keepZ;
 
+    [HideIf("NotChangeAll")][SerializeField] private Vector3    _position;
+    private bool NotChangeAll => !(_keepX && _keepY && _keepZ);
+    
+    [SerializeField] private bool _useLocalPosition;
+
+    private void Start()
+    {
+        if (NotChangeAll) _position = new Vector3(_xValue, _yValue, _zValue);
+    }
     private void Update()
     {
         Vector3 newPosition;
         
-        if (_localPosition) newPosition = transform.localPosition;
+        if (_useLocalPosition) newPosition = transform.localPosition;
         else newPosition = transform.position;
 
         if (_keepX) newPosition.x = _position.x;
         if (_keepY) newPosition.y = _position.y;
         if (_keepZ) newPosition.z = _position.z;
 
-        if (_localPosition) transform.localPosition = newPosition;
+        if (_useLocalPosition) transform.localPosition = newPosition;
         else transform.position = newPosition;
     }
 }
