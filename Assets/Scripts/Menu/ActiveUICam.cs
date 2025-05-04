18618edf228b708b4b3c4ab93648e31574beb11f
notCroptu <martinsmariana1972@gmail.com>
2025-05-04 14:30:18 +0100

    Pass ui rendering to overlay camera so it's not affected by retro and dithering

---
+++ Assets/Scripts/Menu/ActiveUICam.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ActiveUICam : MonoBehaviour
+{
+    [field:SerializeField] public static Camera ActiveUICamera { get; private set; }
+
+    private void OnEnable()
+    {
+        if ( ActiveUICamera == null )
+            ActiveUICamera = GetComponent<Camera>();
+    }
+
+    private void OnDisable()
+    {
+        if (ActiveUICamera == GetComponent<Camera>())
+            ActiveUICamera = null;
+    }
+}
