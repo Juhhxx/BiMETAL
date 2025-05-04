3811bc6f578f9a196bc3cb2624e008c69be0369d
Julia Costa <juliacosta232005@gmail.com>
2025-05-04 05:19:47 +0100

    Add EnemyAttack Script

---
+++ Assets/Scripts/Arena/AI/EnemyAttack.cs
@@ -0,0 +1,57 @@
+using System;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharController))]
+public class EnemyAttack : MonoBehaviour
+{
+    [Header("Attack Colliders Parameters")]
+    [Space(5)]
+    [SerializeField] private CollisionDetector  _attackCollider;
+
+    [Space(10)]
+    [Header("Attack Events")]
+    [Space(5)]
+    public UnityEvent OnSlash;
+
+    // Game Object Components 
+    private CharController  _character;
+
+    private void Start()
+    {
+        _character      = GetComponent<CharController>();
+    }
+    private void OnEnable()
+    {
+        _attackCollider.OnCollisionEnter        += DoAttack;
+    }
+    private void OnDisable()
+    {
+        _attackCollider.OnCollisionEnter        -= DoAttack;
+    }
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            OnSlash?.Invoke();
+        }
+    }
+    private void DoAttack(object sender, OnCollisionEventArgs e)
+    {
+        Collider other = e.other;
+        CharController otherChar = other.gameObject.GetComponent<CharController>();
+
+        if (otherChar != null)
+        {
+            _character.GiveDamage(otherChar, _character.Character.BaseAttack);
+        }
+
+        Debug.Log($"Collision detected with {e.other.gameObject.name} from {gameObject.name}");
+    }
+}
