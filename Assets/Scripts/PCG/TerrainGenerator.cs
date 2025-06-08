392e0b16402d928bac2abace2c263e9ec77ed2cf
notCroptu <martinsmariana1972@gmail.com>
2025-06-08 03:58:29 +0100

    add euclidean falloff

--- Assets/Scripts/PCG/TerrainGenerator.cs
+++ Assets/Scripts/PCG/TerrainGenerator.cs
@@ -9,6 +9,7 @@ public class TerrainGenerator : MonoBehaviour
 
     [SerializeField, Range(0f, 1f)] private float _outsideFalloffStart;
     [SerializeField, Range(0f, 1f)] private float _outsideFalloffEnd;
+    [SerializeField, Range(0f, 1f)] private float _outsideEuclideanAxial = 0.5f;
 
     [SerializeField, Range(0f, 1f)] private float _insideFalloffStart;
     [SerializeField, Range(0f, 1f)] private float _insideFalloffEnd;
@@ -31,22 +32,27 @@ public class TerrainGenerator : MonoBehaviour
                 verts = perlin.Generate(verts, norms, offset);
         }
 
-        _meshFilter.mesh.vertices = GenerateFalloff(verts, norms);
+        verts = GenerateBorderFalloff(verts, norms);
+        _meshFilter.mesh.vertices = GenerateBorderFalloff(verts, norms);
     }
 
-    private Vector3[] GenerateFalloff(Vector3[] vertices, Vector3[] normals)
+    private Vector3[] GenerateBorderFalloff(Vector3[] vertices, Vector3[] normals)
     {
         float s = float.NegativeInfinity;
         for (int i = 0; i < vertices.Length; i++)
         {
             if ( normals[i].z < 0.99f ) continue;
-            if (vertices[i].z > s) s = vertices[i].z;
+            float distance = new Vector2(vertices[i].x, vertices[i].z).magnitude;
+
+            if (distance > s)
+               s = distance;
         }
 
         float min = s * _outsideFalloffStart;
         float max = s * _outsideFalloffEnd;
 
-        Debug.Log("out min and max:  " + max + " " + min);
+        // float maxAxial = float.MinValue;
+        // float maxEucl = float.MinValue;
 
         for (int i = 0; i < vertices.Length; i++)
         {
@@ -68,18 +74,42 @@ public class TerrainGenerator : MonoBehaviour
                 Mathf.Abs(cubeZ)
             );
 
+            // if (distance > maxAxial)
+            //    maxAxial = distance;
+
+            float adjustedAxial = distance * 0.675f;
+
             // Lerp from 0 to 1 in the farthest outside fallout to nearest outside fallout
-            float xlaw = distance - min;
-            if ( xlaw >= 0 )
-            {
-                xlaw /= max;
-                vertices[i].y *= 1f - Mathf.SmoothStep(0f, 1f, Mathf.Clamp01(xlaw));
-            }
+            float t1 = Mathf.InverseLerp(min, max, adjustedAxial);
+            float axial = 1f - Mathf.SmoothStep(0f, 1f, t1);
+
+
+            distance = new Vector2(vertices[i].x, vertices[i].z).magnitude;
+
+            // if (distance > maxEucl)
+            //    maxEucl = distance;
+
+            float t2 = Mathf.InverseLerp(min, max, distance); // 0 inside,1 at edge
+            float euclidean = 1f - Mathf.SmoothStep(0f, 1f, t2);
+
+            vertices[i].y *= Mathf.Lerp(euclidean, axial, _outsideEuclideanAxial);
         }
+        // Debug.Log("Max eucl: " + maxEucl + " max axial: " + maxAxial + " max: " + max);
+
+        return vertices;
+    }
+
+    private Vector3[] GenerateNavFalloff(Vector3[] vertices, Vector3[] normals)
+    {
+        // create an array of nav mesh points (based on _insideFalloffStart)
+        // for each vertice iterate to see which navmesh border points are close enough
+        // use euclidean distance to apply all their height corrections
 
         for (int i = 0; i < vertices.Length; i++)
         {
             if ( normals[i].y < 0.99f ) continue;
+
+            Vector3 v = vertices[i];
         }
 
         return vertices;
